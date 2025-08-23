38c1e995f4126b7b78499e1d1607f3a8f29f9003
Flan-dev1 <clarencem.123@gmail.com>
2025-08-23 15:28:48 +0800

    feat: add slow time cause I felt like it

--- player_movement.gd
+++ player_movement.gd
@@ -7,11 +7,25 @@ extends CharacterBody2D
 func get_input():
 	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	
-	if Input.is_key_pressed(KEY_SHIFT):  # yung slow strafe type shi ni ZUN
+	
+	if Input.is_action_pressed('ui_shift'):  # yung slow strafe type shi ni ZUN
 		velocity = input_direction * speed * slow_modifier
 	else:
 		velocity = input_direction * speed
+	
+	if Input.is_action_just_pressed('ui_shift'):
+		slow_time(true)
+	
+	if Input.is_action_just_released('ui_shift'):
+		slow_time(false)
+
 
 func _physics_process(_delta):
 	get_input()
 	move_and_slide()
+
+func slow_time(isSlow):
+	if isSlow:
+		Engine.time_scale = 0.5
+	else:
+		Engine.time_scale = 1
